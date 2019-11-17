0d0577dedc372c0488bde4302f52f8df53db734d
pandurijal <pandurijal96@gmail.com>
2019-11-17 19:28:35 +0700

    Updates

---
+++ precache-manifest.be4d50679d6f79cad2bcee4c1929d555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de41e92e5321a22db3e8a5aa81f613ef",
+    "url": "/webcam-prediction/index.html"
+  },
+  {
+    "revision": "c9bae0de1f84a78f3618",
+    "url": "/webcam-prediction/static/css/main.c3ff1515.chunk.css"
+  },
+  {
+    "revision": "5d9492d27e948d86f55c",
+    "url": "/webcam-prediction/static/js/2.1c71c113.chunk.js"
+  },
+  {
+    "revision": "c9bae0de1f84a78f3618",
+    "url": "/webcam-prediction/static/js/main.b46c2c61.chunk.js"
+  },
+  {
+    "revision": "5a8974bfa99f388fcdd3",
+    "url": "/webcam-prediction/static/js/runtime~main.700630a3.js"
+  }
+]);
